c0214210efddf72b4a35a980c8e3c42a53df2f15
Matt Gioe <mattgioe@Matts-MacBook.local>
2016-05-28 14:06:23 -0400

    renamed a module to remove whitespace and then added it back in

---
+++ On The ClockTests/PaymentGeneratorTests.swift
@@ -0,0 +1,28 @@
+//
+//  PaymentGeneratorTests.swift
+//  On The Clock
+//
+//  Created by Matt Gioe on 5/28/16.
+//  Copyright © 2016 Matt Gioe. All rights reserved.
+//
+
+import XCTest
+@testable import PaymentGenerator
+
+class PaymentGeneratorTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testInitializePaymentGenerator() {
+        let currentPaymentGenerator = PaymentGenerator{}
+    }
+
+}
